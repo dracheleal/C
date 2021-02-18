69e8e6034fd1d8d055baf1e1c7bd44acf9f4332b
Fer Leal <78566393+dracheleal@users.noreply.github.com>
2021-02-18 14:51:36 -0600

    Update 02Temperature F -°C.c

--- 02Temperature F -°C.c
+++ 02Temperature F -°C.c
@@ -5,9 +5,9 @@ int main()
     int fahr,celsius;
     int lower, upper, step;
     
-    lower = 0; /* Límite inferior de la tabla de temperaturas */
-    upper = 300; /* Límite superior */
-    step = 20; /* Tamaño del incremento */
+    lower = 0;
+    upper = 300; 
+    step = 20; 
     
     fahr = lower;
     while (fahr<=upper)
