7cb632f6331a4206d42f33aac8c4da566887c5d3
Fer Leal <78566393+dracheleal@users.noreply.github.com>
2021-03-04 15:18:25 -0600

    Update homework2.c

--- homework2.c
+++ homework2.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 	 int n, i, j, aux, k;
 	 
-	 printf("introduzca la cantidad de terminos primos que desea saber");
-	 scanf("%i", &n);
+	 printf ("Introduzca la cantidad de terminos primos que desea saber");
+	 scanf  ("%i", &n);
 	 
-	 for(i = 1; i < n; i++){
+	 for (i = 1; i < n; i++)
+	 {
 	 	k = 0;
-	 	for(j = 2; j < i; j++){
+	 	for(j = 2; j < i; j++)
+		{
 	 		aux = i % j;
-	 		if (aux == 0){
+	 		if (aux == 0)
+			{
 	 			k = 1;
-			 }
+			}
 		 }
-		 if (k != 1){
+		 if (k != 1)
+		 {
 		 	printf ("%i, ", i);
 		 }
 	 }
